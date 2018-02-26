41830ea74682e45eabf5d345ffaf0609cfa3c15b
Vladislav Ilkovec <ilkovec.vi@gmail.com>
2018-02-26 13:14:37 +0300

    Zeros: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let countZeroes = 0;
+  let counter = 1;
+  while (true) {
+    let zeroes = Math.floor(number / Math.pow(5, counter));
+    if (zeroes < 1) {
+      break;
+    }
+    countZeroes += zeroes;
+    counter++;
+  }
+  return countZeroes;
 }
